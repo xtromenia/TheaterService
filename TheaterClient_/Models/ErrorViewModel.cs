5f016110f6650dcb048344ea928505015dd8ef96
Kenny Gustavsson <xtromenia@gmail.com>
2021-12-22 17:36:37 +0100

    :P

---
+++ TheaterClient_/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheaterClient_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
