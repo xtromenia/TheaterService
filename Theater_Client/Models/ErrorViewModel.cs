1c1aca1b4e1f15ef9d33a37c59b81dfa4a0a80a1
Kenny Gustavsson <43953889+xtromenia@users.noreply.github.com>
2022-01-03 13:45:33 +0100

    Delete Theater_Client directory
    deleted old client that wasn't used.

--- Theater_Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Theater_Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
